41216db3d7d09187b386dbe0983e9415213e863c
Vinicius Monteiro <vinicius3_1998@hotmail.com>
2025-02-18 14:06:13 -0300

    lab 4

---
+++ src/test/java/com/vinicius/springboot3lab4/Springboot3lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.springboot3lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3lab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
